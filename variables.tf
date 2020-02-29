0baa346c943f58a6dc73ceaade032106df7c0474
aviator-47 <gswastik1996@gmail.com>
2020-02-29 20:22:17 +0530

    Parameterised client ID and secret.

--- variables.tf
+++ variables.tf
@@ -7,3 +7,12 @@ variable "nodepool_types" {
     stdf8sv2 = "Standard_F8s_v2"
   }
 }
+
+variable "client_id" {
+  description = "Service principle client ID."
+  default = "f8bc4caf-4f94-4964-9a3d-6921fbabf7e9"
+}
+
+variable "client_secret" {
+  description = "Service principle client secret."
+}
