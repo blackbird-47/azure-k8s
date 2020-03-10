160c286919a74f7c34fb48f4988b94291af4db4f
aviator-47 <gswastik1996@gmail.com>
2020-03-11 01:13:27 +0530

    Made agent pool creation dynamic.

--- aks.tf
+++ aks.tf
@@ -5,13 +5,13 @@ data "azurerm_resource_group" "swastik-rg" {
 data "azurerm_subnet" "aks-subnet" {
   name = "swastik-aks-2"
   virtual_network_name = "swastik-aks-vnet"
-  resource_group_name = "${data.azurerm_resource_group.swastik-rg.name}"
+  resource_group_name = data.azurerm_resource_group.swastik-rg.name
 }
 
 resource "azurerm_kubernetes_cluster" "aks-multi-nodepool" {
   name = "swastik-aks-2"
-  location = "${data.azurerm_resource_group.swastik-rg.location}"
-  resource_group_name = "${data.azurerm_resource_group.swastik-rg.name}"
+  location = data.azurerm_resource_group.swastik-rg.location
+  resource_group_name = data.azurerm_resource_group.swastik-rg.name
   dns_prefix = "aks-multi-nodepool"
   kubernetes_version = "1.14.7"
 
@@ -27,61 +27,34 @@ resource "azurerm_kubernetes_cluster" "aks-multi-nodepool" {
     load_balancer_sku = "Standard"
   }
 
-  agent_pool_profile {
-    name = "stdds2v2"
-    count = "2"
-    vm_size = "Standard_DS2_v2"
-    os_type = "Linux"
-    os_disk_size_gb = 50
-    vnet_subnet_id = "${data.azurerm_subnet.aks-subnet.id}"
-    availability_zones = "${list("1","2","3")}"
-    enable_auto_scaling = "1"
-    type = "VirtualMachineScaleSets"
-    min_count = "2"
-    max_count = "3"
-    max_pods = "30"
-  }
-
-  agent_pool_profile {
-    name = "stdf4sv2"
-    count = "1"
-    vm_size = "Standard_F4s_v2"
-    os_type = "Linux"
-    os_disk_size_gb = 50
-    vnet_subnet_id = "${data.azurerm_subnet.aks-subnet.id}"
-    availability_zones = "${list("1","2","3")}"
-    enable_auto_scaling = "1"
-    type = "VirtualMachineScaleSets"
-    min_count = "1"
-    max_count = "2"
-    max_pods = "30"
-  }
-
-  agent_pool_profile {
-    name = "stdf8sv2"
-    count = "1"
-    vm_size = "Standard_F8s_v2"
-    os_type = "Linux"
-    os_disk_size_gb = 50
-    vnet_subnet_id = "${data.azurerm_subnet.aks-subnet.id}"
-    availability_zones = "${list("1","2","3")}"
-    enable_auto_scaling = "1"
-    type = "VirtualMachineScaleSets"
-    min_count = "1"
-    max_count = "2"
-    max_pods = "30"
-    node_taints = "${list("cpu=highest_8c:NoExecute","memory=highest_16g:NoExecute")}"
+  dynamic agent_pool_profile {
+    for_each = var.agent_pools
+    content {
+      vnet_subnet_id = data.azurerm_subnet.aks-subnet.id
+      availability_zones = list("1","2","3")
+      name = agent_pool_profile.value.name
+      count = agent_pool_profile.value.count
+      enable_auto_scaling = agent_pool_profile.value.enable_auto_scaling
+      type = agent_pool_profile.value.type
+      min_count = agent_pool_profile.value.min_count
+      max_count = agent_pool_profile.value.max_count
+      max_pods = agent_pool_profile.value.max_pods
+      vm_size = agent_pool_profile.value.vm_size
+      os_type = "Linux"
+      os_disk_size_gb = "40"
+      node_taints = agent_pool_profile.value.node_taints
+    }
   }
 
   service_principal {
-    client_id = "${var.client_id}"
-    client_secret = "${var.client_secret}"
+    client_id = var.client_id
+    client_secret = var.client_secret
   }
 
   addon_profile {
     oms_agent {
       enabled = true
-      log_analytics_workspace_id = "${azurerm_log_analytics_workspace.aks-multi-nodepool.id}"
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.aks-multi-nodepool.id
     }
   }
 
@@ -94,18 +67,18 @@ resource "azurerm_kubernetes_cluster" "aks-multi-nodepool" {
 
 resource "azurerm_log_analytics_workspace" "aks-multi-nodepool" {
   name = "swastik-aks-2-workspace"
-  location = "${data.azurerm_resource_group.swastik-rg.location}"
-  resource_group_name = "${data.azurerm_resource_group.swastik-rg.name}"
+  location = data.azurerm_resource_group.swastik-rg.location
+  resource_group_name = data.azurerm_resource_group.swastik-rg.name
   sku = "PerGB2018"
   retention_in_days = "30"
 }
 
 resource "azurerm_log_analytics_solution" "aks-multi-nodepool" {
   solution_name = "ContainerInsights"
-  location = "${data.azurerm_resource_group.swastik-rg.location}"
-  resource_group_name = "${data.azurerm_resource_group.swastik-rg.name}"
-  workspace_resource_id = "${azurerm_log_analytics_workspace.aks-multi-nodepool.id}"
-  workspace_name = "${azurerm_log_analytics_workspace.aks-multi-nodepool.name}"
+  location = data.azurerm_resource_group.swastik-rg.location
+  resource_group_name = data.azurerm_resource_group.swastik-rg.name
+  workspace_resource_id = azurerm_log_analytics_workspace.aks-multi-nodepool.id
+  workspace_name = azurerm_log_analytics_workspace.aks-multi-nodepool.name
 
   plan {
     publisher = "Microsoft"
--- variables.tf
+++ variables.tf
@@ -1,6 +1,5 @@
 variable "nodepool_types" {
   description = "Nodepool types for k8s cluster."
-  type = "map"
   default = {
     stdds2v2 = "Standard_DS2_v2"
     stdf4sv2 = "Standard_F4s_v2"
@@ -15,4 +14,42 @@ variable "client_id" {
 
 variable "client_secret" {
   description = "Service principle client secret."
+}
+
+variable "agent_pools" {
+  description = "AKS Agent pool configuration variables."
+  default = [
+    {
+      name = "small"
+      count = "3"
+      vm_size = "Standard_DS2_v2"
+      enable_auto_scaling = "1"
+      type = "VirtualMachineScaleSets"
+      min_count = "2"
+      max_count = "3"
+      max_pods = "10"
+      node_taints = []
+    },
+    {
+      name = "medium"
+      count = "2"
+      vm_size = "Standard_F4s_v2"
+      enable_auto_scaling = "1"
+      type = "VirtualMachineScaleSets"
+      min_count = "1"
+      max_count = "2"
+      max_pods = "15"
+      node_taints = ["cpu=medium:NoExecute","memory=medium:NoExecute"]
+    },
+    {
+      name = "large"
+      count = "2"
+      vm_size = "Standard_F8s_v2"
+      enable_auto_scaling = "1"
+      type = "VirtualMachineScaleSets"
+      min_count = "1"
+      max_count = "2"
+      max_pods = "15"
+      node_taints = ["cpu=high:NoExecute","memory=high:NoExecute"]
+    }]
 }
