f41c5be9aa2dc3cb8ce106dd87662f4a48ecbbae
aviator-47 <gswastik1996@gmail.com>
2020-03-14 20:36:02 +0530

    Reformatted acc to v0.12

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "kube_config" {
-  value = "${element(azurerm_kubernetes_cluster.aks-multi-nodepool.*.kube_config_raw,0)}"
+  value = element(azurerm_kubernetes_cluster.aks-multi-nodepool.*.kube_config_raw,0)
 }
