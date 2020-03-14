c8bfedd4bad65f9a6dd3d4d318fb5c261d1cda61
aviator-47 <gswastik1996@gmail.com>
2020-03-14 20:37:20 +0530

    Added account vars.

--- variables.tf
+++ variables.tf
@@ -1,15 +1,13 @@
-variable "nodepool_types" {
-  description = "Nodepool types for k8s cluster."
-  default = {
-    stdds2v2 = "Standard_DS2_v2"
-    stdf4sv2 = "Standard_F4s_v2"
-    stdf8sv2 = "Standard_F8s_v2"
-  }
+variable "subscription_id" {
+  description = "Azure subscription ID."
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID."
 }
 
 variable "client_id" {
   description = "Service principle client ID."
-  default = "f8bc4caf-4f94-4964-9a3d-6921fbabf7e9"
 }
 
 variable "client_secret" {
